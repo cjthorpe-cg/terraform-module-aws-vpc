4f4343f424f6537a0cb252cd8b025c295243eea2
Chris Thorpe <55049521+cjthorpe@users.noreply.github.com>
2019-10-21 14:43:23 +0100

    Update to ensure compat. w/ TF Cloud.

--- variables.tf
+++ variables.tf
@@ -1,36 +1,47 @@
-variable "profile" {
-  description = "The AWS profile to utilise."
-}
-
-variable "region" {
-  description = "The region to build infrastructure within."
-  default = "eu-west-2"
-}
-
-variable "workspace" {
-  description = "The name of the workspace hosting the VPC."
+variable "owner" {
+  default     = ""
+  description = "Identifying name of the resource owner."
+  type        = string
 }
 
 variable "prefix" {
+  default     = ""
   description = "The naming prefix to use."
-}
-
-variable "owner" {
-  description = "Identifying name of the resource owner."
-}
-
-variable "vpc_cidr" {
-  description = "The CIDR range to use for the VPC."
-}
-
-variable "public_subnet_cidr" {
-  description = "The public subnet CIDRs to use."
-  type = list
-  default = []
+  type        = string
 }
 
 variable "private_subnet_cidr" {
+  default     = []
   description = "The private subnet CIDRs to use."
-  type = list
-  default = []
+  type        = list
+}
+
+variable "profile" {
+  default     = ""
+  description = "The AWS profile to utilise."
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  default     = []
+  description = "The public subnet CIDRs to use."
+  type        = list
+}
+
+variable "region" {
+  default     = ""
+  description = "The region to build infrastructure within."
+  type        = list
+}
+
+variable "vpc_cidr" {
+  default     = ""
+  description = "The CIDR range to use for the VPC."
+  type        = string
+}
+
+variable "workspace" {
+  default     = ""
+  description = "The name of the workspace hosting the VPC."
+  type        = string
 }
